27123f07990f3422dcf1386aa3c953ce45b9b289
Ronit Mishra <ronnie94official@gmail.com>
2018-10-06 18:24:58 +0530

    demonstration
    this module!

--- import_this.py
+++ import_this.py
@@ -1,3 +1,3 @@
 import this
 
-# https://stackoverflow.com/questions/5855758/what-is-the-source-code-of-the-this-module-doing?__s=dirvdy6ij69fzpca5nvi
+# Readmore: https://stackoverflow.com/questions/5855758/what-is-the-source-code-of-the-this-module-doing?__s=dirvdy6ij69fzpca5nvi
